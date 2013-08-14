05b850a545e1f8dfc3724cfa3cf0ab9a24876e2c
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-08-14 14:41:55 -0400

    Use the UI to control the functionality

--- app.js
+++ app.js
@@ -13,31 +13,43 @@ Crawler.prototype.addFetchCondition = function(callback) {
     }
 };
 
-// Create a crawler and set up some options
-var myCrawler = new Crawler("health.usf.edu", "/nursing");
-myCrawler.interval = 2000;
-myCrawler.maxConcurrency = 1;
+function initializeCrawler(options) {
+    // Create a crawler and set up some options
+    var myCrawler = new Crawler(options.server, options.path);
+    myCrawler.interval = options.interval;
+    myCrawler.maxConcurrency = options.concurrency;
 
-// Ignore resources that we don't care about
-var conditionID = myCrawler.addFetchCondition(function(parsedURL) {
-    return !parsedURL.uriPath.match(/\.css$/i) &&
-        !parsedURL.uriPath.match(/\.js$/i) &&
-        !parsedURL.uriPath.match(/\.pdf$/i) &&
-        !parsedURL.uriPath.match(/\.jpg$/i) &&
-        !parsedURL.uriPath.match(/\.jpeg$/i) &&
-        !parsedURL.uriPath.match(/\.png$/i) &&
-        !parsedURL.uriPath.match(/\.gif$/i) &&
-        !parsedURL.uriPath.match(/\.bmp$/i);
+    // Ignore resources that we don't care about
+    var conditionID = myCrawler.addFetchCondition(function(parsedURL) {
+        return !parsedURL.uriPath.match(/\.css$/i) &&
+            !parsedURL.uriPath.match(/\.js$/i) &&
+            !parsedURL.uriPath.match(/\.pdf$/i) &&
+            !parsedURL.uriPath.match(/\.jpg$/i) &&
+            !parsedURL.uriPath.match(/\.jpeg$/i) &&
+            !parsedURL.uriPath.match(/\.png$/i) &&
+            !parsedURL.uriPath.match(/\.gif$/i) &&
+            !parsedURL.uriPath.match(/\.bmp$/i);
+    });
+
+    // Every time a page is fetched, so something magical with it
+    myCrawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
+        console.log("I just received %s (%d bytes)",queueItem.url,responseBuffer.length);
+        console.log("It was a resource of type %s",response.headers['content-type']);
+
+        // Do something with the data in responseBuffer
+        var $ = cheerio.load(responseBuffer);
+        console.log($('a').length);
+    });
+
+    myCrawler.start();
+}
+
+// Handle interaction with the user interface
+$('#crawl-begin').on('click', function () {
+    initializeCrawler({
+        server: $('#crawl-url').val(),
+        path: '/',
+        interval: 2000,
+        concurrency: 1
+    });
 });
-
-// Every time a page is fetched, so something magical with it
-myCrawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
-    console.log("I just received %s (%d bytes)",queueItem.url,responseBuffer.length);
-    console.log("It was a resource of type %s",response.headers['content-type']);
-
-    // Do something with the data in responseBuffer
-    var $ = cheerio.load(responseBuffer);
-    console.log($('a').length);
-});
-
-myCrawler.start();
